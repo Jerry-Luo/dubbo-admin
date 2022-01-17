006b7f31257f22e27ea8c5028f502e25e245db5c
LuoJianwei <luojianwei@pinming.cn>
2022-01-17 19:34:09 +0800

    add properties

--- dubbo-admin-server/src/main/java/org/apache/dubbo/admin/utils/JwtTokenUtil.java
+++ dubbo-admin-server/src/main/java/org/apache/dubbo/admin/utils/JwtTokenUtil.java
@@ -20,7 +20,6 @@ package org.apache.dubbo.admin.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
